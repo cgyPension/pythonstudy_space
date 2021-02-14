c5720416f5a008e78246701e7904735e66be5f03
cgyPension <67064353+cgyPension@users.noreply.github.com>
2021-02-14 16:32:28 +0800

    更新

--- 01_base/02_公共方法.py
+++ 01_base/02_公共方法.py
@@ -10,3 +10,19 @@ print({1, 2, 3} - {3})
 print('hello' * 3)
 print([1, 2, 3] * 3)
 print((1, 2, 3) * 3)
+
+# in：成员运算符
+print('a' in 'abc')
+print(1 in [1, 2, 3])
+print(4 in (6, 4, 5))
+
+# in 用于字典是用来判断key是否存在
+print('zhangsan' in {'name': 'zhangsan', 'age': 18, 'height': '180cm'})
+print('name' in {'name': 'zhangsan', 'age': 18, 'height': '180cm'})
+print(3 in {3, 4, 5})
+
+nums = [19, 82, 39, 12, 34, 58]
+# 带下标的遍历 enumerate 类的使用，一般用户列表和元组等有序的数据
+en = enumerate(nums)
+for i, e in en:
+    print('第%d个数据是%d'%(i, e))
