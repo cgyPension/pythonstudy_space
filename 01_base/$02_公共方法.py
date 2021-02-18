66cd5482f678f86688adb6d98553627a2bba6bf0
pensioncai <pensioncai@can-dao.com>
2021-02-18 18:06:13 +0800

    更新

--- 01_base/$02_公共方法.py
+++ 01_base/$02_公共方法.py
@@ -80,6 +80,7 @@ print(m2)  # <re.Match object; span=(12, 16), match='good'>
 print(m2.pos, m2.endpos)  # 开始位置结束位置
 print(m2.span())
 
+# . 表示除了换行符以外的任意字符
 # 任意字符 * 出现任意次数 贪婪模式
 m6 = re.search(r'w.*m', 'hello wrold good morining')
 print(m6.group())  # 获取匹配到的字符结果
@@ -95,6 +96,12 @@ print(m7.group(3))
 
 print(m7.groups())  # 以元组显示所有分组
 
+# (?P<name>表达式) 可以给分组起一个名字
+# groupdict 获取分组组成的字典
+m8 = re.search(r' (9.*) (?P<xxx>0.*) (5.*7)', 'da9fioriel5kfsda7ifsdaiferit')
+print(m8.groupdict('xxx'))
+print(m7.group(1))
+print(m7.group('xxx'))  # 可以通过分组名或者分组的下标获取到分组里匹配到的字符串
 
 # finditer 查找到所有的匹配对象，返回的结果是一个可迭代对象,是一个re.Match 类型的对象
 # findall  把查找到的所有字符结果都放到一个列表里
@@ -109,3 +116,157 @@ print(m4)
 
 m5 = re.fullmatch(r'hello', 'hello wrold')
 print(m5)
+
+# 在re模块里，可以使用re. 方法调用函数，还可以调用re.compile得到一个对象
+# 正则修饰符是对正则表达式进行修饰
+# . 表示除了换行符以外的任意字符
+# re.S 让点 . 匹配换行
+# re.I 忽略大小写
+# re.M 让$ 能够匹配到换行
+q = re.search(r'm.*a', 'orgsadg\nslsa', re.S)
+print(q)
+
+r = re.compile(r'm.*a')
+x = r.search('orgsadgnslsa')
+y = r.search('asoflio\nbghapga')
+print(x)
+
+# \w:表示的是字母数字和_ +:出现一次以上 $:以指定得内容结尾
+# \w 等价于[0-9a-zA-Z]
+# \W 取反
+z = re.findall(r'\w+$', 'tam boy\n you are girl\n he is man')
+
+# 1. 很多字母前面添加 \ 会有特殊含义
+# 2. 绝大多数标点符号都有特殊含义
+# 3. 如果想要使用标点符号 可以加 \
+
+# \s 表示任意的空白字符（非打印字符）
+# \S 表示非空白字符
+# \n:表示换行 \t:表示一个制表符
+# \d:表示数字 等价于 [0-9]
+# \D:表示非数字 等价于 [^0-9]
+print(re.search(r'\s', 'hello word\n666\t'))
+print(re.search(r'x\d+p', 'x534p'))
+
+# () 表示一个分组
+g = re.search(r'h(\d+)x', 'sh829xkflsa')
+print(g.group(0))
+print(g.group(1))
+
+# 如果要表示括号，需要使用 \
+g1 = re.search(r'\(.*\)', '(1+1)*3+5')
+print(g1.group())
+
+# [] 用来表示可选项 [x-y]从x到y区间，包含x和y
+print(re.search(r'f[0-5]m', 'odsf0m'))
+print(re.search(r'f[05d]m', 'odsf0m'))  # 0或5或或d
+print(re.search(r'f[0-5]+m', 'odsf0487m'))
+print(re.search(r'f[0-5a-dx]m', 'odsf0dm'))  # 0-5或a-d或x
+
+# | 用来表示或  和 [] 有一定的区别 这里只是取值
+# [] 里的值表示区间，而且是单个字符
+# | 就是可选值，可以出现多个值
+print(re.search(r'f(x|prz|t)m', 'odsfprzm'))
+
+# {n} 用来限定前面元素出现的次数
+# {n,} 表示前面的元素出现 n 次以上
+# {,n} 表示前面的元素出现 n 次以下
+# *:表示前面的元素出现任意次数(0次及以上) 等价于{0,}
+# +:表示前面的元素至少出现一次,等价于{1,}
+print(re.search(r'go{2}d', 'good'))
+print(re.search(r'go{2,}d', 'gooooood'))
+print(re.search(r'go{,2}d', 'god'))
+print(re.search(r'go*d', 'gooooood'))
+print(re.search(r'go+d', 'god'))
+
+# ?:两种用法:
+# 1.规定前面的元素最多只能出现一次,等价{,1}
+# 2.将贪婪模式转换成为非贪婪模式  r'a.*?i'
+print(re.search(r'go?d', 'god'))
+
+# ^ 以指定得内容开头 $ 指定内容结尾
+# ^ 在[] 里面还可以表示取反
+print(re.search(r'^a.*i$', 'adsi sodihaofdi'))
+
+'''
+# 判断用户输入的内容是否是数字，如果是数字转换成为数字类型
+num = input('请输入一段数字：')
+if re.fullmatch(r'\d+(\.?\d+)?',num): # 3.12.84.35
+    print('数字')
+    print(float(num))
+else:
+    print('不是一个数字')
+    
+# 用户名匹配 1.用户名只能包含数字、字母和下划线 2.不能以数字开头 3.长度在6到6位范围
+username = input('请输入用户名：')
+x = re.fullmatch(r'[a-zA-Z_][a-zA-Z0-9_]{5,15}',username)
+if x is None:
+    print('输入的用户名不符合规范')
+else:
+    print(x)
+'''
+
+# 以非数字开头，后面有 字母数字_-组成的长度4到14位的字符串
+# r'^\D[a-z0-9A-Z_\-]{3,13}'
+
+# 匹配邮箱
+# r'^([A-Za-z0-9_\-\.])+@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$'
+
+# 匹配手机
+# r'^(13[0-9])|(14[5|7)|(15([0-3]|[5-9]))|(18[05-9]))\d{8}$'
+
+
+# 替换 sub
+print(re.sub(r'\d', 'x', 'gasfkg46456gijdag1369'))
+
+
+def test():
+    y = int(x.group(0))
+    y *=2
+    return str(y)
+
+p = 'gasfkg46456gijdag1p9'
+# sub 内部在调用 test 方法时，会把每一个匹配到的数据以re.Match的格式
+print(re.sub(r'\d+', test, p))  # test函数是自动调用
+
+# TODO ======================================== 网络 ========================================
+print('======================================== 网络 ========================================')
+import socket
+# 1.创建socket,并连接
+# AF_INET:表示这个socket是用来进行网络连接
+# SOCK_DGRAM:表示连接是一个 TODO udp连接 只管发 不安全 没有严格的客户端和服务器的区别
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# 2.发送数据
+# data:要发送的数据，它是二进制的数据
+# address:发送给谁，参数是一个元祖,元祖里有两个元素
+# 第0个表示目标的ip地址，第1个表示程序的端口号
+s.sendto('hello'.encode('utf8'),('192.168.31.199',9000))
+data,addr = s.recvfrom(1024)  # recufrom是一个阻塞的方法,等待
+print('接收到了{}地址{}端口号接收到了消息,内容是:{}'.format(addr[0],addr[1],data).decode('utf-8'))
+s.close()  # 3.关闭socket
+
+'''
+# 接受数据 绑定端口号和ip
+s.bind(('192.168.31.199',9000))
+# revfrom 接受数据
+data,addr = s.recvfrom(1024) #recufrom是一个阻塞的方法,等待
+print('从{}地址{}端口号接收到了消息,内容是:{}'.format(addr[0],addr[1],data).decode('utf-8'))
+s.close()
+'''
+
+# SOCK_DGRAM:表示连接是一个
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# TODO tcp连接 要先和服务器建立连接才能发送数据 更安全
+s.connect(('192.168.31.199',9000))
+s.send('hello'.encode('utf8'))
+s.close()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.listen(128)  # 把socker监听
+s.close()
+
+
+
+# TODO ======================================== 网络 ========================================
+print('======================================== 网络 ========================================')
