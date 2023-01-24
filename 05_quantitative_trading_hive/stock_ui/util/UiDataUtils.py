136fbd5524c27a603011446f299e6e34345fc445
大C <2855227591@qq.com>
2023-01-24 16:58:07 +0800

    更新

--- 05_quantitative_trading_hive/stock_ui/util/UiDataUtils.py
+++ 05_quantitative_trading_hive/stock_ui/util/UiDataUtils.py
@@ -1,6 +1,6 @@
 import os
 import sys
-# 在linux会识别不了包 所以要加临时搜索目录
+# 在linux会识别不了包 所以要加临时搜索目
 curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
 sys.path.append(rootPath)
